f9adbbdcf01fdb2d39ad19f9c13cd8d92dc77b68
Miltiadis Parcharidis 011873 <>
2024-11-02 00:31:31 +0200

    Just testing, first files to the Git

---
+++ internal/config.php
@@ -0,0 +1,10 @@
+<?php
+// Basic connection settings
+$servername = '172.16.64.209';
+$username = 'adise';
+$password = 'adise';
+$dbname = 'adise2024';
+
+// Connect to the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
